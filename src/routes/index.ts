e0b505461927e473ee9a793c0169839c9d0fcf24
TKAkhter <talhaakhter01@gmail.com>
2024-08-26 19:20:59 +0500

    shows module removed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,6 @@ import userRouter from "./users";
 import authRouter from "./auth";
 import taskRouter from "./tasks";
 import { healthCheckRouter } from "../controller/index.controller";
-import showsRouter from "./shows";
 
 const router = Router();
 
@@ -13,6 +12,5 @@ router.use("/v1/healthcheck", healthCheckRouter);
 router.use("/v1/users", userRouter);
 router.use("/v1/auth", authRouter);
 router.use("/v1/tasks", taskRouter);
-router.use("/v1/shows", showsRouter);
 
 export default router;
